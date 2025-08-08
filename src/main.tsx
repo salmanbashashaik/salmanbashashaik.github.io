997ca728798320b9501790cc109ecc1b47b5fb25
salmanbashashaik <salmanbasha.shaikk@gmail.com>
2025-08-08 17:12:44 -0300

    init

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-
+import { HashRouter } from "react-router-dom";
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+      </HashRouter>
+  </BrowserRouter  >
   </React.StrictMode>
 );
